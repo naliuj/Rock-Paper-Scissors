dd8807aa2ff19a063aa06c8ee22d751bbd32a159
naliuj <naliuj@users.noreplu.github.com>
2017-03-16 13:10:19 -0400

    instatiated p1 and cpu in Game class as well as importing the compare method

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,5 +1,21 @@
 package game;
 
-public class Game {
+import java.io.Serializable;
+
+public class Game implements Serializable {
+    
+    private PersonPlayer p1 = new PersonPlayer();
+    private ComputerPlayer cpu = new ComputerPlayer();
+    
+    public void compare(int pMove) {
+        p1.generateMove(pMove);
+        cpu.generateMove();
+        
+        if (p1.compareTo(cpu) == 1) {
+            p1.incrementWins();
+        } else if (p1.compareTo(cpu) == -1) {
+            cpu.incrementWins();
+        }
+    }
     
 }
