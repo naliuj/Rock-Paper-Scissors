da8de4c3bb66691b040b834eb2f523196832a2a0
naliuj <naliuj@users.noreplu.github.com>
2017-03-20 11:49:39 -0400

    implemented winMessage string and getWinMessage method in Game

--- src/game/Game.java
+++ src/game/Game.java
@@ -7,15 +7,24 @@ public class Game implements Serializable {
     public PersonPlayer p1 = new PersonPlayer();
     public ComputerPlayer cpu = new ComputerPlayer();
     
+    private String winMessage = "";
+       
     public void compare(int pMove) {
         p1.generateMove(pMove);
         cpu.generateMove();
         
         if (p1.compareTo(cpu) == 1) {
             p1.incrementWins();
+            winMessage = p1.getName() + " wins!";
         } else if (p1.compareTo(cpu) == -1) {
             cpu.incrementWins();
+            winMessage = cpu.getName() + " wins!";
         }
+        winMessage = "It's a tie!";
+    }
+    
+    public String getWinMessage() {
+        return winMessage;
     }
     
     public Game() {
